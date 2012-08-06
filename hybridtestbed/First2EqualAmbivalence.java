14a7f3c3cd5dadc5003013486e8f86dda31ddf9a
Nicole Nelson <nicole.nelson8489@gmail.com>
2012-08-06 11:06:53 -0400

    updated to utilize DataInfo

--- hybridtestbed/First2EqualAmbivalence.java
+++ hybridtestbed/First2EqualAmbivalence.java
@@ -12,34 +12,37 @@ public class First2EqualAmbivalence extends Ambivalence {
 	}
 
 	@Override
-	public boolean isAmbivalent() {
-		if (heap.hasActivityValues()) {
+	public boolean isAmbivalent(DataInfo dataInfo) {
+		if (heap.hasActivityValues() &&
+				(maintainPercent && 
+				(dataInfo.getWalksatTime() / (System.currentTimeMillis() - dataInfo.getSolverStartTime())) * 100 < percent
+				|| !maintainPercent)) {
 			isAmbivalentCalls++;
 			//if (random.nextInt(scoutProb) == 0) {
-			System.out.println("check equality");
+			//System.out.println("check equality");
 				if (heap.size() >= 2
 						&& ((activities[0] = heap.getActivity(vals[0] = heap.getmin())) == (activities[1] = heap
 								.getActivity(vals[1] = heap.getmin())))) {
 					//evaluateAmbivalence++;
-					System.out.println("passed equality check");
-					System.out.println(activities[0] + " -- " + activities[1]);
+					//System.out.println("passed equality check");
+					//System.out.println(activities[0] + " -- " + activities[1]);
 					double sum = 0;
 					int numItems = 2;
 					sum += activities[0] + activities[1];
 					for (int i = 2; i < 10 && heap.size() > 0; i++) {
 						vals[i] = heap.getmin();
 						activities[i] = heap.getActivity(vals[i]);
-						System.out.println(activities[i]);
+						//System.out.println(activities[i]);
 						sum += activities[i];
 						if (activities[i] > activities[i - 1]) {
 							System.exit(1);
 						}
 						numItems++;
 					}
-					System.out.println("normalized values " + sum);
+					//System.out.println("normalized values " + sum);
 					for (int j = 0; j < numItems; j++) {
 						activities[j] = activities[j] / sum;
-						System.out.println(activities[j]);
+						//System.out.println(activities[j]);
 						heap.insert(vals[numItems - j - 1]);
 					}
 					
@@ -47,7 +50,7 @@ public class First2EqualAmbivalence extends Ambivalence {
 						// return random.nextInt(scoutProb) == 0;
 						evaluateAmbivalence++;
 						boolean returnVal = random.nextInt(scoutProb) == 0;
-						System.out.println("less than ambProb " + returnVal);
+						//System.out.println("less than ambProb " + returnVal);
 						firstGuy += activities[0];
 						if (activities[0] < minFirstGuy) {
 							minFirstGuy = activities[0];
